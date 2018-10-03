fb2705c6eb645626bd5d25661a14c4d0c36a82a2
Augustin Juricic <augustin.juricic@gmail.com>
2018-10-03 14:19:14 +0200

    Cleanup and fix.

--- src/dashboard/dashboard.app.ts
+++ src/dashboard/dashboard.app.ts
@@ -1,9 +1,14 @@
 import { UrlGenerator } from 'generators'
+import { DashService } from './dashboard.service';
 
 export class DashApp {
     private generator: UrlGenerator;
+    private service: DashService;
+
     constructor() {
         this.generator = new UrlGenerator();
+        this.service = new DashService();
         this.generator.build(1);
+        this.service.sayHello(2);
     }
 }
